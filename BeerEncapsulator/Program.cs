b2c1c5b2bf75b3756258b4b6bdb42391e4dd6700
JGouyau <Gouyau.jimmy@gmail.com>
2021-09-27 11:05:47 +0200

    create project

---
+++ BeerEncapsulator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerEncapsulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
